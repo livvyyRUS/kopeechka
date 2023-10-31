b2b91c6cf96b3f0598815740e3ee9fd20d368eda
livvyy <94890179+livvyyRUS@users.noreply.github.com>
2023-10-31 22:59:14 +0300

    Add files via upload

--- test/log_test.py
+++ test/log_test.py
@@ -1,5 +1,9 @@
 from kopeechka import MailActivations
+import logging
 
 api = MailActivations("TOKEN")
+
+logger = logging.getLogger("kopeechka")
+logger.setLevel(logging.CRITICAL + 1)
 
 print(api.user_balance().balance)
